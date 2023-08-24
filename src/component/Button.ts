f46fe2c875de38ebf02eb6d8a770e52a52f8b8bf
denis <anywaymaybe1@gmail.com>
2023-08-24 20:24:14 +0400

    init

---
+++ src/component/Button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Button-comp',
+  templateUrl: './button.html',
+  styleUrls: ["./button.scss"]
+})
+export class ButtonComponent {
+  title = 'angularTestApp';
+}
